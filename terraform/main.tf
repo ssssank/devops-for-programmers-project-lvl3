d71d7db0f6a951151d94b6e40970b639cb046711
Aleksandr Litvinov <ssssank@gmail.com>
2022-11-06 21:12:42 +0500

    add cert

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,13 +39,30 @@ resource "digitalocean_loadbalancer" "loadbalancer" {
     target_protocol = "http"
   }
 
+  forwarding_rule {
+    entry_port     = 443
+    entry_protocol = "http"
+
+    target_port     = 3000
+    target_protocol = "http"
+
+    certificate_name = digitalocean_certificate.cert.name
+  }
+
   healthcheck {
     port     = 3000
     protocol = "http"
     path = "/"
   }
 
+
   droplet_ids = [digitalocean_droplet.web1.id, digitalocean_droplet.web2.id]
+}
+
+resource "digitalocean_certificate" "cert" {
+  name    = "le-terraform-example"
+  type    = "lets_encrypt"
+  domains = ["devops-hexlet.lol"]
 }
 
 resource "digitalocean_database_firewall" "database_firewall_1" {
